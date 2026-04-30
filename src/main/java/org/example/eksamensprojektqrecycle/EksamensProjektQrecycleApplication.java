c893e9aac49fd63f07577c70dd719275efb3e1d0
SebastianMunt <semu0001@stud.kea.dk>
2026-04-30 13:31:48 +0200

    first commit

---
+++ src/main/java/org/example/eksamensprojektqrecycle/EksamensProjektQrecycleApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojektqrecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensProjektQrecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensProjektQrecycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eksamensprojektqrecycle/EksamensProjektQrecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojektqrecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensProjektQrecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
