d02ba9ec54492a2834bae9906e3352af0a88ee84
SebastianMunt <semu0001@stud.kea.dk>
2026-05-08 12:57:25 +0200

    noget

--- src/main/resources/sqlSripts/1.0-opret.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE restauranter (
-                              id INT AUTO_INCREMENT PRIMARY KEY,
-                              navn VARCHAR(255) NOT NULL,
-                              adresse VARCHAR(255) NOT NULL
-);
