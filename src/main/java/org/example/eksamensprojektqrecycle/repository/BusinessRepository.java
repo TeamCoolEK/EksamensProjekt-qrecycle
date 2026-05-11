e401353747bfbb56f942b13d0f784505a598975c
Anoepus <ansu0001@stud.kea.dk>
2026-05-11 09:31:43 +0200

    Command line runner til dummy data + spring security config

--- src/main/java/org/example/eksamensprojektqrecycle/repository/BusinessRepository.java
+++ src/main/java/org/example/eksamensprojektqrecycle/repository/BusinessRepository.java
@@ -1,7 +1,9 @@
 package org.example.eksamensprojektqrecycle.repository;
 
+import org.example.eksamensprojektqrecycle.model.entity.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BusinessRepository {
+public interface BusinessRepository extends JpaRepository<Business, Integer> {
 }
--- src/main/java/org/example/eksamensprojektqrecycle/repository/RouteRepository.java
+++ src/main/java/org/example/eksamensprojektqrecycle/repository/RouteRepository.java
@@ -1,9 +1,9 @@
 package org.example.eksamensprojektqrecycle.repository;
 
-import org.example.eksamensprojektqrecycle.model.entity.Collection;
+import org.example.eksamensprojektqrecycle.model.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CollectionRepository extends JpaRepository<Collection, Integer> {
+public interface RouteRepository extends JpaRepository<Route, Integer> {
 }
