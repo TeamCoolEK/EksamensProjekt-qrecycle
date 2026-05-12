ec4615fd5d5bfb9308845c5bbca6128f83a77533
SebastianMunt <semu0001@stud.kea.dk>
2026-05-12 11:49:01 +0200

    QE-37

--- src/main/java/org/example/eksamensprojektqrecycle/repository/CollectionRepository.java
+++ src/main/java/org/example/eksamensprojektqrecycle/repository/CollectionRepository.java
@@ -1,7 +1,17 @@
 package org.example.eksamensprojektqrecycle.repository;
 
+import org.example.eksamensprojektqrecycle.model.entity.Collection;
+import org.example.eksamensprojektqrecycle.model.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CollectionRepository {
+public interface CollectionRepository extends JpaRepository<Collection, Integer> {
+
+    @Query("SELECT SUM(c.businessBags) FROM Collection c WHERE c.status = :status")
+    Integer sumBusinessBagsByStatus(@Param("status") Status status);
+
+    Integer Status(Status status);
 }
--- src/main/java/org/example/eksamensprojektqrecycle/service/StatisticService.java
+++ src/main/java/org/example/eksamensprojektqrecycle/service/StatisticService.java
@@ -1,7 +1,25 @@
 package org.example.eksamensprojektqrecycle.service;
 
+import org.example.eksamensprojektqrecycle.model.enums.Status;
+import org.example.eksamensprojektqrecycle.repository.CollectionRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticService {
+
+private final CollectionRepository collectionRepository;
+
+public StatisticService(CollectionRepository collectionRepository) {
+    this.collectionRepository = collectionRepository;
+}
+
+public int getBagsReadyForPickup(){
+
+    Integer bags = collectionRepository.sumBusinessBagsByStatus(Status.KLAR);
+    if(bags == null){
+        return 0;
+    }
+    return bags;
+}
+
 }
